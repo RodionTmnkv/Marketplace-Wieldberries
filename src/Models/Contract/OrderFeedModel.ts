212a4f275f1b0b6b9d8be0e93e6c3acb9b336016
RodionTmnkv <rdntmnkv@mail.ru>
2022-06-21 18:48:36 +0300

    push project

---
+++ src/Models/Contract/OrderFeedModel.ts
@@ -0,0 +1,17 @@
+import { OrderType } from '@/Models/Enums';
+
+export type OrderFeedModel = {
+  id: number;
+  barcode: string;
+  date: string;
+  delivery_region: string;
+  discount: number;
+  dispatch_warehouse: string;
+  order_id: number;
+  order_type: OrderType;
+  product_name: string;
+  size: string;
+  quantity: number;
+  total_price: number;
+  nm_id: number;
+};
